61a9c7815cfaa5ed5d8b8fb245c83b7509642435
Karmela <karmela.sliskovic@gmail.com>
2018-04-13 16:17:08 +0200

    Added few fixes and letters

---
+++ Assets/Script/EnableLetter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableLetter : MonoBehaviour
+{
+    public GameObject Letter;
+
+    private void Start()
+    {
+        Letter.SetActive(true);
+    }
+
+    public void OnMouseDown()
+    {
+        Letter.SetActive(false);
+    }
+
+}
